2bc7d2b1104fdfb655beeac2d0093e73156ac544
JLineB <jaqueline.brsilva@gmail.com>
2021-04-24 15:46:03 -0300

    Construção da tela, UserIdentification

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+// import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 
-export default function App() {
+export default function App(){
+
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+    return <AppLoading/>
+
   return (
-    <Welcome/>
+    <UserIdentification/>
   );
 }
