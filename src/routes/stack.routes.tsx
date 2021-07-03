e38c5648fe1c7fdb2addc9201b79e4a70b15b9e7
JLineB <jaqueline.brsilva@gmail.com>
2021-07-03 11:29:34 -0300

    Insert and Update 'Auth Routes'

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,9 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
-import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 import colors from '../styles/colors';
 
 const stackRoutes = createStackNavigator();
@@ -37,7 +36,7 @@ const AppRoutes: React.FC = () => (
 
         <stackRoutes.Screen
             name="PlantSelect"
-            component={PlantSelect}
+            component={AuthRoutes}
         />
 
         <stackRoutes.Screen
@@ -47,7 +46,7 @@ const AppRoutes: React.FC = () => (
 
         <stackRoutes.Screen
             name="MyPlants"
-            component={MyPlants}
+            component={AuthRoutes}
         />
 
     </stackRoutes.Navigator>
