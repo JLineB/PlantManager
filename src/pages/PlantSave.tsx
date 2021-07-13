b68450c846dff6249e609d49036ba5bbb9dd7fdb
JLineB <jaqueline.brsilva@gmail.com>
2021-07-13 17:07:03 -0300

    Navegação para página 'Confirmation'

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -49,6 +49,14 @@ export function PlantSave() {
                 dateTimeNotification: selectedDateTime
             });
 
+            navigation.navigate('Confirmation', {
+                title: 'Tudo certo',
+                subtitle: 'Fique tranquilo que sempre vamos lembrar você de cuidar da sua plantinha com muito cuidado',
+                buttonTitle: 'Muito Obrigado :D',
+                icon: 'hug',
+                nextScreen: 'MyPlants'
+            });
+
         } catch {
             Alert.alert('Não foi possível salvar 😢');
         }
