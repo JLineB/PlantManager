45ab23ef65ddd4e154d2e566bbf59d755f1e1694
JLineB <jaqueline.brsilva@gmail.com>
2021-09-07 16:25:52 -0300

    Ajustes no 'server' para Deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.13:3333',
+    baseURL: 'https://my-json-server.typicode.com/JLineB/PlantManager',
 });
 
 export default api;
